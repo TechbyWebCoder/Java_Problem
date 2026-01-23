d3096baf6555c50fb5993dbd72a0c5cb565c4222
TechbyWebCoder <techbywedcoder@gmail.com>
2026-01-23 19:01:48 +0530

    Add ArraySum program to calculate sum of array

---
+++ ArraySum Problem/ArraySum.java
@@ -0,0 +1,13 @@
+// Program 03
+// ArraySum Problem
+
+class ArraySum{
+ public static void main(String[] a){
+  int[] x={1,2,3,4};
+  int s = 0;
+  for(int i:x){
+    s+=i;
+  }
+  System.out.print(s);
+ }
+}
