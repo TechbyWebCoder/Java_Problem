baef2d5dfa6d031f45003e19cd8732e26cddf6ab
TechbyWebCoder <techbywedcoder@gmail.com>
2026-01-28 18:09:48 +0530

    Add Java program to calculate factorial of 10

---
+++ Factorial Problem/Factorial.java
@@ -0,0 +1,12 @@
+// Program 08
+// Factorial Problem
+
+class Factorial{
+ public static void main(String[] a){
+  int n = 10, f = 1;
+  for(int i = 1; i<=n; i++){
+    f*=i;
+  }
+  System.out.print(f);
+ }
+}
