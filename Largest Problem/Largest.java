bb014e0e9b45e540c2cd1c8e940e8594788edf9e
TechbyWebCoder <techbywedcoder@gmail.com>
2026-01-30 18:13:00 +0530

    Add Largest.java to find the largest of three numbers

---
+++ Largest Problem/Largest.java
@@ -0,0 +1,11 @@
+// Program 10
+// Largest Problem
+
+class Largest{
+ public static void main(String[] a){
+  int x=10, y=25, z=20;
+  int m=x>y?(x>z?x:z):(y>z?y:z);
+
+  System.out.println(m);
+ }
+}
